aa7cb0139c5dc09e5b7b21176466f553b57fb4af
VictorMouraz <jvtm@ecomp.poli.br>
2019-07-11 14:35:04 -0300

    adicionando a estrutura projeto spring

---
+++ tw-gerenciador-tarefas/src/main/java/br/com/treinaweb/twgerenciadortarefas/TwGerenciadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.twgerenciadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwGerenciadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwGerenciadorTarefasApplication.class, args);
+	}
+
+}
---
+++ tw-gerenciador-tarefas/src/test/java/br/com/treinaweb/twgerenciadortarefas/TwGerenciadorTarefasApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.treinaweb.twgerenciadortarefas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwGerenciadorTarefasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
