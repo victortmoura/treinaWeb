62ac0137110f9b5ee2c0b95ab9ad800acc307a3c
VictorMouraz <jvtm@ecomp.poli.br>
2019-07-15 09:47:44 -0300

    Ajustes TarefaController e novas paginas html

--- tw-gerenciador-tarefas/src/main/java/br/com/treinaweb/twgerenciadortarefas/controllers/TarefasController.java
+++ tw-gerenciador-tarefas/src/main/java/br/com/treinaweb/twgerenciadortarefas/controllers/TarefasController.java
@@ -1,11 +1,19 @@
 package br.com.treinaweb.twgerenciadortarefas.controllers;
 
+import java.util.Date;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.treinaweb.twgerenciadortarefas.modelos.Tarefa;
 import br.com.treinaweb.twgerenciadortarefas.repositorios.RepositorioTarefa;
 
 @Controller
@@ -21,6 +29,64 @@ public class TarefasController {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("tarefas/listar");
 		mv.addObject("tarefas", repositorioTarefa.findAll());
+		return mv;
+	}
+
+	@GetMapping("/inserir")
+	public ModelAndView inserir() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("tarefas/inserir");
+		mv.addObject("tarefa", new Tarefa());
+		return mv;
+	}
+
+	@PostMapping("/inserir")
+	public ModelAndView inserir(@Valid Tarefa tarefa, BindingResult result) {
+		ModelAndView mv = new ModelAndView();
+		if (tarefa.getDataExpiracao() == null) {
+			result.rejectValue("dataExpiracao", "tarefa.dataExpiracaoInvalida",
+					"A data de expiração é obrigatória.");
+		}else if (tarefa.getDataExpiracao().before(new Date())) {
+			result.rejectValue("dataExpiracao", "tarefa.dataExpiracaoInvalida",
+					"A data de expiração não pode ser anterior a data atual.");
+		}
+		if (result.hasErrors()) {
+			mv.setViewName("tarefas/inserir");
+			mv.addObject(tarefa);
+		} else {
+			mv.setViewName("redirect:/tarefas/listar");
+			repositorioTarefa.save(tarefa);
+		}
+		return mv;
+	}
+	
+	@GetMapping("/alterar/{id}")
+	public ModelAndView alterar(@PathVariable("id") Long id) {
+		ModelAndView mv = new ModelAndView();
+		Tarefa tarefa = repositorioTarefa.getOne(id);
+		mv.addObject("tarefa", tarefa);
+		
+		return mv;
+		
+	}
+	
+	@PostMapping("/alterar")
+	public ModelAndView alterar(@Valid Tarefa tarefa, BindingResult result) {
+		ModelAndView mv = new ModelAndView();
+		if (tarefa.getDataExpiracao() == null) {
+			result.rejectValue("dataExpiracao", "tarefa.dataExpiracaoInvalida",
+					"A data de expiração é obrigatória.");
+		}else if (tarefa.getDataExpiracao().before(new Date())) {
+			result.rejectValue("dataExpiracao", "tarefa.dataExpiracaoInvalida",
+					"A data de expiração não pode ser anterior a data atual.");
+		}
+		if (result.hasErrors()) {
+			mv.setViewName("tarefas/alterar");
+			mv.addObject(tarefa);
+		} else {
+			mv.setViewName("redirect:/tarefas/listar");
+			repositorioTarefa.save(tarefa);
+		}
 		return mv;
 	}
 }
--- tw-gerenciador-tarefas/src/main/java/br/com/treinaweb/twgerenciadortarefas/modelos/Tarefa.java
+++ tw-gerenciador-tarefas/src/main/java/br/com/treinaweb/twgerenciadortarefas/modelos/Tarefa.java
@@ -8,23 +8,31 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "tar_tarefas")
 public class Tarefa {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "tar_id")
 	private Long id;
 
 	@Column(name = "tar_titulo", length = 50, nullable = false)
+	@NotNull(message = "O título é obrigatório")
+	@Length(max = 50, min = 3, message = "O título deve conter entre 3 e 50 caracteres")
 	private String titulo;
 
 	@Column(name = "tar_descricao", length = 100, nullable = true)
+	@Length(max = 100, message = "A descrição deve conter até 100 caracteres")
 	private String descricao;
 
 	@Column(name = "tar_data_expiracao", nullable = false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date dataExpiracao;
 
 	@Column(name = "tar_concluida", nullable = false)
